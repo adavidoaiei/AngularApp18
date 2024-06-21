9060f5b713a3bbb1ba28d84b964e0feb470e82b3
adavidoaiei <cornel.adavidoaiei@gmail.com>
2024-06-21 11:01:37 +0300

    change array

--- AngularApp18.Server/Controllers/WeatherForecastController.cs
+++ AngularApp18.Server/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AngularApp18.Server.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Cornel", "Raluca", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
